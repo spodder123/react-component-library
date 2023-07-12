9a1d30d69103ee0b0009ed1296d5110c4a9a2c5a
spodder123 <120617408+spodder123@users.noreply.github.com>
2023-07-12 15:12:20 -0500

    testing husky

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { type ButtonProps } from './Button.types';
-
+// test
 const StyledButton = styled.button<ButtonProps>`
   border: 0;
   line-height: 1;
